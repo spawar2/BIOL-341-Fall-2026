a1fcf63e16059a3192d541ac2c93686348929bcd
Pawar <pawar1550@gmail.com>
2026-08-28 20:22:50 +0530

    Add files via upload

---
+++ Microarray-Analysis 2.R
@@ -0,0 +1,32 @@
+#Author: Pawar, Date: 08/26/2026, Purpose: Microarray analysis
+
+# Install microarray affy package
+
+if (!require("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+
+BiocManager::install("affy")
+
+# Load the affy package
+
+library(affy)
+
+# Set the working directory for R
+
+setwd("/Users/pawar/Desktop")
+
+# Read the Affymetrix dataset in R
+
+data <- ReadAffy()
+
+# Plot the read dataset as a boxplot
+
+boxplot(data)
+
+
+
+
+
+
+
+
