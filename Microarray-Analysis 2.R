4a30d64b10ed832cc18b0f9a6023563967acd256
Pawar <pawar1550@gmail.com>
2026-09-04 19:02:47 +0530

    Add files via upload

--- Microarray-Analysis 2.R
+++ Microarray-Analysis 2.R
@@ -40,12 +40,24 @@ Control <- Normalizeddata[,c(3,4)]
 
 # Taking averages on rows for Treatment and Control so we have 1 column for Treatment and 1 column for Control
 
-
+Treatment_Mean <- rowMeans(Treatment)
+Control_Mean <- rowMeans(Control)
 
 # Fold change analysis for determining significant genes, Up-regulation and Down-regulation of genes
-# Fold changes can be performed using subtraction and division
+# Fold changes can be performed using subtraction or division
 
-Fold_Changes <- Treatment - Control
+Fold_Changes <- Treatment_Mean - Control_Mean
+
+# Any fold change value >2 is an up-regulated genes and any fold change value <-2 is an down-regulated genes. Any gene between 2 and -2 is an insignificantly expressed gene
+
+   100_g_at     1000_at     1001_at   1002_f_at 
+ 0.02004328 -0.37687887  0.31352888  0.23368661 
+  1003_s_at     1004_at 
+ 0.07965329  0.08178445 
+ 
+ 
+
+
 
 
 
