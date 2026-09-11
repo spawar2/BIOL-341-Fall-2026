5c15903f1ab02167052ddf3c95434b57fffaec43
Pawar <pawar1550@gmail.com>
2026-09-11 20:20:38 +0530

    Add files via upload

--- Phylogenetic-Analysis.R
+++ Phylogenetic-Analysis.R
@@ -1,20 +1,13 @@
-#Author: Pawar; Date: September 18, 2024; Purpose: Create dummy data, generate phylogenetic tree plot.
+#Author: Pawar, Date: 09/11/2026, Purpose: Phyologenetic analysis 
 
-# Load tree libraries
+# Load the 4 libraries
 library(ape)
+library(phangorn)
+library(phytools)
+library(geiger)
+"A,B,C,D,Z"
+# Create some dummy string dataset
 
-# Generate some dummy data
-text.string<- "(((((((cow, pig),whale),(bat,(lemur,human))),(robin,iguana)),coelacanth),gold_fish),shark);"
+text.string<- "(((((((cow, pig),whale),(bat,(lemur,human))),(robin,iguana)),coelacanth),gold_fish),shark);" 
 
-# Read the dummy data as a tree structure
 vert.tree<-read.tree(text=text.string)
-
-# Generate a basic plot of dummy data
-plot(vert.tree,no.margin=TRUE,edge.width=2, main="Phylogenetic tree")
-
-# Generate a un-rooted plot on the dummy data
-plot(unroot(vert.tree),type="unrooted",no.margin=TRUE,lab4ut="axial", edge.width=2)
-
-
-
-
